14b5f60d92e0426e04403c9abde631e4ac0af22b
Amaan <dksh7024@gmail.com>
2025-08-08 14:00:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,7 @@ st.markdown(
     <style>
     .full-width-banner {
         width: 100vw;
-        height: 240px;
+        height: 300px;
         background-image: url('Spectraa.AI/LDMDk.png'); 
         background-size: cover;
         background-position: center;
